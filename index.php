d5ceb6724cf023ce15bc8d236b0faeb5af42baae
my-name <email@server.com>
2023-05-29 05:24:56 +0700

    - Change meet levels

--- index.php
+++ index.php
@@ -84,7 +84,8 @@ page_top ();
 <div>
 	<p>
 		
-		<a href="?cat=connect30">MEET</a>&nbsp;&nbsp;
+		<a href="?cat=connect30_close">MEET_1</a>&nbsp;&nbsp;
+		<a href="?cat=connect30_loose">MEET_2</a>&nbsp;&nbsp;
 		<a href="?cat=maintain&type=countdown">MAINTAIN</a>&nbsp;&nbsp;
 		<a href="?cat=events&type=countdown">EVENT</a>&nbsp;&nbsp;
 		<a href="?cat=wishlist">WISH</a>&nbsp;&nbsp;
