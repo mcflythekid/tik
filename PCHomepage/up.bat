4ddbf6d921f9df6d081af07c60c36f0d3cc20387
my-name <email@server.com>
2023-12-28 17:53:36 +0700

    -- add homepage

---
+++ PCHomepage/up.bat
@@ -0,0 +1,3 @@
+scp ./index.html heo:/home/mc/app/tik/PCHomepage/
+scp ./style.css heo:/home/mc/app/tik/PCHomepage/
+scp ./fa.png heo:/home/mc/app/tik/PCHomepage/
---
+++ up_home.cmd
@@ -0,0 +1,1 @@
+scp -r PCHomepage heo:~/app/tik/
